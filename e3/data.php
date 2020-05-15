780350429a90da428a106040fde19e55c1c823ce
Kausthub Adidam <60486124+sripadh97@users.noreply.github.com>
2020-05-15 10:38:37 -0400

    Update data.php

--- e3/data.php
+++ e3/data.php
@@ -3,7 +3,7 @@
 function get_skills($name)
 {
         /* Database INFO */
-	$servername = "localhost";
+	$servername = "cs.newpaltz.edu";
 	$username = "adidamv1";
 	$password = "xv1td6";
 	$dbname = "adidamv1_db";
