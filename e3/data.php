8d27f1d211ed0882035f4cad26dbe1730ff1204e
Kausthub Adidam <60486124+sripadh97@users.noreply.github.com>
2020-05-17 12:33:28 -0400

    Update data.php

--- e3/data.php
+++ e3/data.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_skills($name)
+function get_Skills($name)
 {
         /* Database INFO */
 	$servername = "ec2-3-235-24-81.compute-1.amazonaws.com";
@@ -15,21 +15,21 @@ function get_skills($name)
             die("Connection failed: " . $conn->connect_error);
        }
 
-       $sql = "SELECT skills FROM Companies WHERE technology='$name'";
+       $sql = "SELECT Skills FROM Enterprise WHERE Technology='$name'";
        $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
              // output data of each row
              while($row = $result->fetch_assoc()) {
-                      $skills = $row["skills"];
+                      $Skills = $row["Skills"];
       }
     } else {
-                     $skills = null;
+                     $Skills = null;
         }
 
     $conn->close();
 
-    return $skills;
+    return $Skills;
 }
 
 ?>
